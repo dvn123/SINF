efad90528b260509bef4824c4d384e82f5d725a8
Tiago Fernandes <ei11054@fe.up.pt>
2014-11-28 23:27:40 +0000

    working on products page

---
+++ app/viewProducts/viewProducts_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.viewProducts module', function() {
+
+    beforeEach(module('myApp.viewProducts'));
+
+    describe('viewProducts controller', function(){
+
+        it('should ....', inject(function($controller) {
+            //spec body
+            var viewProductsCtrl = $controller('viewProductsCtrl');
+            expect(viewProductsCtrl).toBeDefined();
+        }));
+
+    });
+});
