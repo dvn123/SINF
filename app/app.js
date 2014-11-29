c2841638808853c3455ad22f227742df1ef709b1
dvn123 <diogvaznunes@gmail.com>
2014-11-29 18:18:32 +0000

    Added single product page

--- app/app.js
+++ app/app.js
@@ -6,8 +6,10 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.viewProducts',
+  'myApp.viewProduct',
   'myApp.version'
 ]).
+
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
---
+++ app/partials/viewProduct/singleProduct.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('myApp.viewProduct', ['ngRoute'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/products/:productID', {
+            templateUrl: 'partials/viewProduct/single-product.html',
+            controller: 'viewProductCtrl'
+        });
+    }])
+
+    .controller('viewProductCtrl', ['$http', '$scope', '$routeParams' ,function($http, $scope, $routeParams) {
+        $http.get('1.json').success(function(data) {
+            $scope.product = data;
+        });
+    }]);
