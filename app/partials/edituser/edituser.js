f6fd905554d846564fd7fa2bf69994f23c9a45a6
Gabriel Candal <gabrielcandal@gmail.com>
2014-12-09 19:13:30 +0000

    More user info, edit/profile pages.

---
+++ app/partials/edituser/edituser.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('myApp.editUser', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/edituser', {
+                controller: 'editUserCtrl',
+                templateUrl: 'partials/editUserProfile/edituser.html'
+            })
+    }])
+    .controller('editUserCtrl', function ($http, $scope, Auth, $log, $location) {
+        $scope.errorMessage = "";
+        $scope.newuser = {};
+
+        $scope.differentPasswords = function () {
+            return $scope.newuser.password !== $scope.newuser.password2;
+        };
+
+        $scope.editUser = function () {
+            $("#editFormSubmitButton").blur();
+
+            Auth.edit($scope.newuser,
+                function (data) {
+                    $scope.newuser = {};
+                    $location.path('/history');
+                },
+                function (error) {
+                    $scope.errorMessage = error.message;
+                });
+        }
+    });
