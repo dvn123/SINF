c7ae24af9d0eb0356b6e31a7ed4d578547e4abcf
Tiago Fernandes <ei11054@fe.up.pt>
2014-12-16 09:49:40 +0000

    main page products

--- app/partials/home/home.js
+++ app/partials/home/home.js
@@ -1,14 +1,22 @@
 'use strict';
+var link = 'http://127.0.0.1:49822/api/';
 
 angular.module('myApp.home', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'partials/home/home.html',
-    controller: 'HomeCtrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/', {
+        templateUrl: 'partials/home/home.html',
+        controller: 'HomeCtrl'
+      });
+    }])
 
-.controller('HomeCtrl', [function() {
+    .controller('HomeCtrl', [function() {
+      var home = this;
 
-}]);
+      home.hots = [];
+      home.monos = [];
+
+      $http.get(link + 'products/').success(function(data) {
+        home.hots = data;
+      });
+    }]);
